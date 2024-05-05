dfee38924b2ce962965ae1c4b1ca2870b28af2b8
Jonathan Jarvis <jonathan.jarvis@live.co.uk>
2024-05-05 16:03:12 +0100

    Initial 3 challenges finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, repititions) {
+    if (repititions < 0) {
+        return 'ERROR';
+    } else {
+        finalString = '';
+        for (let i = 1; i <= repititions; i++) {
+            finalString = finalString.concat(string);
+        }
+        return finalString;
+    };
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let string = input.split('');
+    let x = string.length;
+    let reverseString = [];
+    for (let i = 0; i < x; i++) {
+        reverseString.push(string.pop());
+    }
+    return reverseString.join("");
 
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+  
