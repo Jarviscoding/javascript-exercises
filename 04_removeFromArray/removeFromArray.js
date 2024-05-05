43a00b760da7929f5337973568198652584ae7e2
Jonathan Jarvis <jonathan.jarvis@live.co.uk>
2024-05-05 23:44:26 +0100

    Created solutions for challenges 4 & 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const finalArray = [];
+    array.forEach((element) => {
+        if (!args.includes(element)){
+            finalArray.push(element);
+        }
+    });
+    return finalArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(numberOne, numberTwo) {
+    let sum = 0;
+    if( numberOne < 0 || numberTwo < 0 ) {
+        return 'ERROR';
+    } else if ( !Number.isInteger(numberOne) || !Number.isInteger(numberTwo) ){
+        return 'ERROR';
+    } else {
+        if( numberOne < numberTwo) {
+            for(let i = numberOne; i <= numberTwo; i++){
+                sum += i;
+            }
+        } else {
+            for(let i = numberTwo; i <= numberOne; i++){
+                sum += i;
+            }
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
